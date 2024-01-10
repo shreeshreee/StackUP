766dcc2bad7d684bf5fbeea22eb8eb6c11607cb8
Tim Krebs <timkrebs@microsoft.com>
2024-01-10 08:44:59 +0100

    init

--- source/backend/app/main.py
+++ source/backend/app/main.py
@@ -1,11 +1,34 @@
 from fastapi import FastAPI, status
+from fastapi.middleware.cors import CORSMiddleware
+
+from . import models
+from .database import engine
+from .routers import post, user, auth, vote
+from .config import settings
 
 
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-#run app: uvicorn main:app --reload
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+#app.include_router(post.router)
+app.include_router(user.router)
+#app.include_router(auth.router)
+#app.include_router(vote.router)
+
+#run app: uvicorn app.main:app --reload
 
 @app.get("/", status_code=status.HTTP_200_OK ,tags=["root"])
 def root():
     return {"message": "Hello StackUP!"}
+
